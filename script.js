6868160e187d163cb4144c557332624bf64f9d44
DanyNaka <daninakassima@gmail.com>
2021-05-24 10:17:58 -0300

    Aula 2 - criado movimento

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let canvas = document.getElementById("snake");
-let context = canvas.getContext("2d");  //renderiza o desenho dentro do canvas
+let context = canvas.getContext("2d"); //renderiza o desenho dentro do canvas
 let box = 32; //32px cada quadrado
 let snake = [];
 
@@ -8,18 +8,41 @@ snake[0] = {
     y: 8 * box
 }
 
-function criarBG(){
+let direction = "right";
+
+function criarBG() {
     context.fillStyle = "lightgreen"; //cor do contrxto
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-function criarCobrinha(){
-    for(i=0; i < snake.length; i++){
+function criarCobrinha() {
+    for (i = 0; i < snake.length; i++) {
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo() {
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);
